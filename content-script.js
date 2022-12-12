63c295afadff079193addec0fe16e38303bc0271
TimoBel <timo.bel@hotmail.com>
2022-12-12 18:49:36 -0300

    added row for whole team avg and now it works for any format of players

--- content-script.js
+++ content-script.js
@@ -3,6 +3,7 @@ window.onload = function(){
 
         var playersTable = (document.getElementsByClassName("Table Table--align-right Table--fixed Table--fixed-left")["0"])
         var statsTable = (document.getElementsByClassName("Table Table--align-right")["1"])
+
         var minutes = document.getElementsByClassName("jsx-2810852873 table--cell stat-min tar");
         var fieldGoalAttemps = document.getElementsByClassName("jsx-2810852873 table--cell stat-fgm/fga tar");
         var freeThrowAttemps = document.getElementsByClassName("jsx-2810852873 table--cell stat-ftm/fta tar");
@@ -14,47 +15,67 @@ window.onload = function(){
         var turnOvers = document.getElementsByClassName("jsx-2810852873 table--cell stat-to tar");
         var points = document.getElementsByClassName("jsx-2810852873 table--cell stat-pts tar");
 
+        var benchsPlayers = document.querySelectorAll('[title="Bench"]');
         
-        function getAvgStat(stat,fixed){
+        
+        function getAvgStat(stat,fixed, benchsPlayersAmmount){
             var avgStat = 0;
-            for(let i =1; i<stat.length-3; i++){
+            for(let i=1; i<stat.length-benchsPlayersAmmount; i++){
                 if(stat[i].innerText != "--"){
-                    avgStat += parseFloat(stat[i].innerText); 
+                    avgStat += parseFloat(stat[i].innerText);                
                 }
                 
             }
-            avgStat = avgStat/10;
+            avgStat = avgStat/(stat.length-(benchsPlayersAmmount+1));           
             return avgStat.toFixed(fixed);
+            // return Math.round((avgStat + Number.EPSILON) *100)/100;
         }
 
-        function getAttemps(stat, type){
+        function getAttemps(stat, type, benchsPlayersAmmount){
             var avgStat = 0
-            for(let i =1; i<stat.length-3; i++){
+            for(let i=1; i<stat.length-benchsPlayersAmmount; i++){
                 if(stat[i].innerText != "--/--"){
                     avgStat += parseFloat(stat[i].innerText.split('/')[type])
                 }    
             }
-            avgStat = avgStat/10;
-            return avgStat.toFixed(1);
-
+            avgStat = avgStat/(stat.length-(benchsPlayersAmmount+1));
+            // return avgStat.toFixed(1);
+            return Math.round((avgStat + Number.EPSILON) *100)/100;
         }
         
-        var avgMinutes = getAvgStat(minutes,1);  
-        var avgFGMade = getAttemps(fieldGoalAttemps,0);
-        var avgFGAttempted = getAttemps(fieldGoalAttemps,1);
-        var avgFTMade = getAttemps(freeThrowAttemps,0);
-        var avgFTAttempted = getAttemps(freeThrowAttemps,1);  
-        var avg3PM = getAvgStat(trheePointsMade, 1);
-        var avgRebounds = getAvgStat(rebounds, 1);
-        var avgAssists = getAvgStat(assists, 1);
-        var avgSteals = getAvgStat(steals, 1);
-        var avgBlocks = getAvgStat(blocks, 1);
-        var avgTO = getAvgStat(turnOvers, 1);
-        var avgPoints = getAvgStat(points, 1);
+        var avgMinutes = getAvgStat(minutes,1, benchsPlayers.length);  
+        var avgFGMade = getAttemps(fieldGoalAttemps,0, benchsPlayers.length);
+        var avgFGAttempted = getAttemps(fieldGoalAttemps,1, benchsPlayers.length);
+        var avgFTMade = getAttemps(freeThrowAttemps,0, benchsPlayers.length);
+        var avgFTAttempted = getAttemps(freeThrowAttemps,1, benchsPlayers.length);  
+        var avg3PM = getAvgStat(trheePointsMade, 1, benchsPlayers.length);
+        var avgRebounds = getAvgStat(rebounds, 1, benchsPlayers.length);
+        var avgAssists = getAvgStat(assists, 1, benchsPlayers.length);
+        var avgSteals = getAvgStat(steals, 1, benchsPlayers.length);
+        var avgBlocks = getAvgStat(blocks, 1, benchsPlayers.length);
+        var avgTO = getAvgStat(turnOvers, 1, benchsPlayers.length);
+        var avgPoints = getAvgStat(points, 1, benchsPlayers.length);
+
+        var avgMinutesTotal = getAvgStat(minutes,1, 0);  
+        var avgFGMadeTotal = getAttemps(fieldGoalAttemps,0, 0);
+        var avgFGAttemptedTotal = getAttemps(fieldGoalAttemps,1, 0);
+        var avgFTMadeTotal = getAttemps(freeThrowAttemps,0, 0);
+        var avgFTAttemptedTotal = getAttemps(freeThrowAttemps,1, 0);  
+        var avg3PMTotal = getAvgStat(trheePointsMade, 1, 0);
+        var avgReboundsTotal = getAvgStat(rebounds, 1, 0);
+        var avgAssistsTotal = getAvgStat(assists, 1, 0);
+        var avgStealsTotal = getAvgStat(steals, 1, 0);
+        var avgBlocksTotal = getAvgStat(blocks, 1, 0);
+        var avgTOTotal = getAvgStat(turnOvers, 1, 0);
+        var avgPointsTotal = getAvgStat(points, 1, 0);
 
         var playersRow = playersTable.insertRow(12);
         var cell1 = playersRow.insertCell(0);
         cell1.innerHTML = "AVG";
+
+        var playersRowTotal = playersTable.insertRow(16);
+        var cell1Total = playersRowTotal.insertCell(0);
+        cell1Total.innerHTML = "AVG Total";
 
         var statsRow = statsTable.insertRow(12);
         var minutesCell = statsRow.insertCell(0);
@@ -69,6 +90,20 @@ window.onload = function(){
         var blkCell = statsRow.insertCell(9);
         var toCell = statsRow.insertCell(10);
         var ptsCell = statsRow.insertCell(11);
+
+        var statsRowTotal = statsTable.insertRow(16);
+        var minutesCellTotal = statsRowTotal.insertCell(0);
+        var fgaCellTotal = statsRowTotal.insertCell(1);
+        var fgCellTotal = statsRowTotal.insertCell(2);
+        var ftaCellTotal = statsRowTotal.insertCell(3);
+        var ftCellTotal = statsRowTotal.insertCell(4);
+        var trheePointCellTotal = statsRowTotal.insertCell(5);
+        var rebCellTotal = statsRowTotal.insertCell(6);
+        var astCellTotal = statsRowTotal.insertCell(7);
+        var stlCellTotal = statsRowTotal.insertCell(8);
+        var blkCellTotal = statsRowTotal.insertCell(9);
+        var toCellTotal = statsRowTotal.insertCell(10);
+        var ptsCellTotal = statsRowTotal.insertCell(11);
     
         minutesCell.innerHTML = avgMinutes;
         fgaCell.innerHTML = avgFGMade+"/"+avgFGAttempted;
@@ -83,8 +118,23 @@ window.onload = function(){
         toCell.innerHTML = avgTO;
         ptsCell.innerHTML = avgPoints;
 
+        minutesCellTotal.innerHTML = avgMinutesTotal;
+        fgaCellTotal.innerHTML = avgFGMadeTotal+"/"+avgFGAttemptedTotal;
+        fgCellTotal.innerHTML = (avgFGMadeTotal/avgFGAttemptedTotal).toFixed(3);
+        ftaCellTotal.innerHTML = avgFTMadeTotal+"/"+avgFTAttemptedTotal;
+        ftCellTotal.innerHTML = (avgFTMadeTotal/avgFTAttemptedTotal).toFixed(3);
+        trheePointCellTotal.innerHTML = avg3PMTotal;
+        rebCellTotal.innerHTML = avgReboundsTotal;
+        astCellTotal.innerHTML = avgAssistsTotal;
+        stlCellTotal.innerHTML = avgStealsTotal;
+        blkCellTotal.innerHTML = avgBlocksTotal;
+        toCellTotal.innerHTML = avgTOTotal;
+        ptsCellTotal.innerHTML = avgPointsTotal;
+
         playersRow.classList.add("playersRow");
         statsRow.classList.add("statsRow");
+        playersRowTotal.classList.add("playersRow");
+        statsRowTotal.classList.add("statsRow");
 
 
     }, 3000);
